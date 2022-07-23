e2d5f3cbb253915f8b76c2ed5b7752801d2c3788
Andreas Tzionis <andy0n7@gmail.com>
2022-07-24 01:52:59 +0300

    updated names

--- scripts/deploy-ledger.sh
+++ scripts/deploy-ledger.sh
@@ -1,6 +1,6 @@
 
 # use private ledger candid interface
-contents="$(jq '.canisters.ledger.candid = "src/ledger/ledger.private.did"' dfx.json)"
+contents="$(jq '.canisters.ledger.candid = "src/_meta/ledger/ledger.private.did"' dfx.json)"
 echo "${contents}" > dfx.json
 
 # deploy ledger canister
@@ -9,5 +9,5 @@ MINTER="34cf47ac751b6393362ba030cfec1d2ff9218f1c901fe990b1e1919b5b902e51" ## a r
 dfx deploy ledger --argument '(record {minting_account = "'${MINTER}'"; initial_values = vec { record { "'${USER}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
 
 # use public ledger candid interface
-contents="$(jq '.canisters.ledger.candid = "src/ledger/ledger.public.did"' dfx.json)"
+contents="$(jq '.canisters.ledger.candid = "src/_meta/ledger/ledger.public.did"' dfx.json)"
 echo "${contents}" > dfx.json
