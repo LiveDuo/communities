02fc48d52c29cbc82cba00e282b919eefeea9c7b
Andreas Tzionis <andy0n7@gmail.com>
2022-08-07 23:17:48 +0300

    cleanup

--- src/_parent/frontend/pages/Home.js
+++ src/_parent/frontend/pages/Home.js
@@ -1,5 +1,5 @@
-import { useContext } from 'react'
-import { Box, Button, Link, Text } from '@chakra-ui/react'
+import { useContext, useEffect, useCallback, useState } from 'react'
+import { Box, Button, Link, Text, useToast } from '@chakra-ui/react'
 import { ParentContext } from '../store/parent'
 import { LedgerContext } from '../store/ledger'
 import { IdentityContext } from '../store/identity'
@@ -12,35 +12,44 @@ import { getAccountId } from '../utils/account'
 
 const Example = () => {
 
-	const { walletConnected, userPrincipal } = useContext(IdentityContext)
-	const { createChild, childPrincipal, parentCanisterId, callCreateCanister, loading, getCreateChildTx } = useContext(ParentContext)
-	const { callAdminBalance, requestTransferICP, requestBalanceICP, ledgerTransferICP, ledgerBalanceICP, getTransferIcpTx } = useContext(LedgerContext)
-
+	const { walletConnected, userPrincipal, ledgerActorPlug } = useContext(IdentityContext)
+	const { childPrincipal, parentCanisterId, loading, getCreateChildTx } = useContext(ParentContext)
+	const { ledgerBalanceICP, getTransferIcpTx } = useContext(LedgerContext)
+	const [balance, setBalance] = useState(null)
+	const toast = useToast()
+	
 	const createChildBatch = async () => {
 		const accountId = getAccountId(parentCanisterId, userPrincipal)
-		await window.ic.plug.batchTransactions([getTransferIcpTx({accountId, amount: 100000000n}), getCreateChildTx()])
+		const transferTx = balance > 0 ? [getTransferIcpTx({accountId, amount: 100000000n})] : []
+		try {
+			await window.ic.plug.batchTransactions([...transferTx, getCreateChildTx()])
+		} catch (error) {
+			const description = error.message ?? 'Transaction failed'
+			toast({ description, status: 'error' })
+		}
 	}
 
+	const getAdminBalance = useCallback(async () => {
+		const _balance = await ledgerBalanceICP(parentCanisterId, userPrincipal)
+		setBalance(_balance)
+	}, [ledgerBalanceICP, parentCanisterId, userPrincipal])
+
+	useEffect(() => {
+		if (ledgerActorPlug) {
+			getAdminBalance()
+		}
+	}, [getAdminBalance, ledgerActorPlug])
+
+	if (!walletConnected) return <Text>Wallet not connected</Text>
 	return (
 		<Box m="20px">
 			<Box mb="20px">
-				<Button onClick={() => requestTransferICP(parentCanisterId, userPrincipal)} disabled={!walletConnected}>Request Transfer ICP</Button>
-				<Button ml="8px" onClick={() => requestBalanceICP()} disabled={!walletConnected}>Request Balance ICP</Button>
-				<Text as="span">&nbsp; ← Looks like they not working for localhost</Text>
+				{balance > 0 && <Text>User Balance: {balance / 1e8} ICP</Text>}
 			</Box>
-			<Box mb="20px">
-				<Button onClick={() => ledgerTransferICP(parentCanisterId, userPrincipal)} disabled={!walletConnected}>Ledger Transfer ICP</Button>
-				<Button ml="8px" onClick={() => ledgerBalanceICP(parentCanisterId, userPrincipal)} disabled={!walletConnected}>Ledger Balance ICP</Button>
-				<Text as="span">&nbsp; ← Not throwing correct errors if ledger canister not found</Text>
+			<Box>
+				<Button mb="8px" isLoading={loading} onClick={() => createChildBatch()}>Create Child</Button>
 			</Box>
-			<Box mb="20px">
-				<Button onClick={callCreateCanister} disabled={!walletConnected}>Call Create Canister</Button>
-				<Button ml="8px" onClick={() => callAdminBalance(parentCanisterId)} disabled={!walletConnected}>Call Admin Balance</Button>
-				<Button ml="8px" onClick={() => createChildBatch()}>Create Child (batch)</Button>
-			</Box>
-
-			<Box mt="140px">
-				<Button isLoading={loading} loadingText='Creating...' onClick={createChild}>Create Child</Button>
+			<Box>
 				<Box>{childPrincipal &&
 					<Link href={getPrincipalUrl(childPrincipal)} isExternal>
 						{childPrincipal} <ExternalLinkIcon mx='2px' />
--- src/_parent/frontend/store/ledger.js
+++ src/_parent/frontend/store/ledger.js
@@ -9,65 +9,17 @@ import { idlLedgerFactory, ledgerCanisterId } from '../agents/ledger'
 
 const LedgerContext = createContext()
 
-const transferAmount = 0.5
-
 const LedgerProvider = ({ children }) => {
 
 	const toast = useToast()
 	const [loading, setLoading] = useState()
 	const { ledgerActorPlug } = useContext(IdentityContext)
 
-	const requestTransferICP = async (parentCanisterId, userPrincipal) => {
-		const accountId = getAccountId(parentCanisterId, userPrincipal)
-		const transferParams = { to: accountId, amount: transferAmount * 1e8 }
-		try {
-			await window.ic?.plug.requestTransfer(transferParams)
-			toast({ description: `Transfer success` })
-		} catch (error) {
-			toast({ description: 'Transfer failed', status: "error" })
-		}
-	}
-
-	const requestBalanceICP = async () => {
-		try {
-			const requestBalanceResponse = await window.ic?.plug.requestBalance()
-			const balance = requestBalanceResponse.find(c => c.symbol === 'ICP').amount
-			toast({ description: `Balance: ${balance} ICP` })
-		} catch (error) {
-			toast({ description: 'Balance failed', status: 'error' })
-		}
-	}
-
-	const ledgerTransferICP = async (parentCanisterId, userPrincipal) => {
-		const accountId = getAccountId(parentCanisterId, userPrincipal)
-		const sendParams = {
-			to: accountId, fee: { e8s: 0n },
-			amount: { e8s: transferAmount * 1e8 }, memo: 0, from_subaccount: [], created_at_time: []
-		}
-		try {
-			await ledgerActorPlug.send_dfx(sendParams)
-			toast({ description: 'Transfer success' })
-		} catch (error) {
-			toast({ description: 'Transfer failed', status: 'error' })
-		}
-	}
-
 	const ledgerBalanceICP = async (parentCanisterId, userPrincipal) => {
 		const accountId = getAccountId(parentCanisterId, userPrincipal)
 		try {
 			const response = await ledgerActorPlug.account_balance_dfx({ account: accountId })
-			toast({ description: `Balance: ${Number(response.e8s) / 1e8} ICP` })
-		} catch (error) {
-			const description = error.result?.reject_message ?? 'Balance failed'
-			toast({ description, status: 'error' })
-		}
-	}
-
-	const callAdminBalance = async (parentCanisterId) => {
-		const accountId = getAccountId(parentCanisterId)
-		try {
-			const response = await ledgerActorPlug.account_balance_dfx({ account: accountId })
-			toast({ description: `Balance: ${Number(response.e8s) / 1e8} ICP` })
+			return Number(response.e8s)
 		} catch (error) {
 			const description = error.result?.reject_message ?? 'Balance failed'
 			toast({ description, status: 'error' })
@@ -90,7 +42,7 @@ const LedgerProvider = ({ children }) => {
 		onFail: (res) => console.log('Error', res)
 	})
 
-	const value = { getTransferIcpTx, requestTransferICP, requestBalanceICP, ledgerTransferICP, ledgerBalanceICP, callAdminBalance, loading, setLoading }
+	const value = { getTransferIcpTx, ledgerBalanceICP, loading, setLoading }
 
 	return <LedgerContext.Provider value={value}>{children}</LedgerContext.Provider>
 }
