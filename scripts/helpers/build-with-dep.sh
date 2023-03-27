499cee82e98c54b659230bf21f810a282b3aca59
Andreas Tzionis <andy0n7@gmail.com>
2023-03-27 18:31:58 +0300

    removed patches

--- scripts/helpers/build-with-dep.sh
+++
@@ -1,3 +0,0 @@
-find target/wasm32-unknown-unknown/release/deps -name 'ic-certified-assets*' -delete
-find target/wasm32-unknown-unknown/release/.fingerprint -name 'ic_certified_assets*' -delete
-cargo build --target wasm32-unknown-unknown --package  parent --release
--- scripts/helpers/replace-env-var.sh
+++
@@ -1,4 +0,0 @@
-
-SEARCH_STR="REACT_APP_CHILD_CANISTER_ID"
-CHILD_ID=$(dfx canister id child)
-sed -i "" "s/$SEARCH_STR/$CHILD_ID/g" "./build/child/static/js/bundle.js"
