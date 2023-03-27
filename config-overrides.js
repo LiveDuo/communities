4154e0d3475e3a15d047c13063b966c5d77fd763
Andreas Tzionis <andy0n7@gmail.com>
2023-03-27 17:22:18 +0300

    updated config overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,35 @@
 const path = require('path')
+const fs = require('fs')
 
 module.exports = {
     webpack: (config) => {
         config.output = {
             ...config.output,
             filename: 'static/js/bundle.js'
+        }
+
+        // clears canister id env var so it's replaced after a new canister is created
+        // NOTE: plugin indexes change between development and production
+        if (process.env.NODE_ENV === 'production' && process.env.DEPLOY_MODE !== 'standalone') {
+            // child canister
+            config.plugins[2].replacements.REACT_APP_CHILD_CANISTER_ID = 'REACT_APP_CHILD_CANISTER_ID'
+            config.plugins[4].definitions['process.env'].REACT_APP_CHILD_CANISTER_ID = '"REACT_APP_CHILD_CANISTER_ID"'
+
+            // parent canister
+            const canisterData = fs.readFileSync('./canister_ids.json', 'utf8')
+            const canisterIds = JSON.parse(canisterData)
+            config.plugins[2].replacements.REACT_APP_PARENT_CANISTER_ID = canisterIds.parent.ic
+            config.plugins[4].definitions['process.env'].REACT_APP_PARENT_CANISTER_ID = `"${canisterIds.parent.ic}"`
+
+            // ledger canister
+            const ledgerCanisterId = 'ryjl3-tyaaa-aaaaa-aaaba-cai' // constant for mainnet
+            config.plugins[2].replacements.REACT_APP_LEDGER_CANISTER_ID = ledgerCanisterId
+            config.plugins[4].definitions['process.env'].REACT_APP_LEDGER_CANISTER_ID = `"${ledgerCanisterId}"`
+
+            // cmc canister
+            const cmcCanisterId = 'rkp4c-7iaaa-aaaaa-aaaca-cai' // constant for mainnet
+            config.plugins[2].replacements.REACT_APP_CMC_CANISTER_ID = cmcCanisterId
+            config.plugins[4].definitions['process.env'].REACT_APP_CMC_CANISTER_ID = `"${cmcCanisterId}"`
         }
         return config
     },
