edfee2642449db32cdcba017cf77e39fd06d5c83
Andreas Tzionis <andy0n7@gmail.com>
2023-04-02 23:45:29 +0300

    updated config overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,47 +1,52 @@
 const path = require('path')
 const fs = require('fs')
 
+const canisterData = fs.readFileSync('.dfx/local/canister_ids.json', 'utf8')
+const canisterIds = JSON.parse(canisterData)
+
+// mainnet canisters
+const LEDGER_CANISTER_ID = 'ryjl3-tyaaa-aaaaa-aaaba-cai'
+const CMC_CANISTER_ID = 'rkp4c-7iaaa-aaaaa-aaaca-cai'
+
+const updateEnvVar = (config, key, value) => {
+    const InterpolateIndex = config.plugins.findIndex(e => e.constructor.name === 'InterpolateHtmlPlugin')
+    config.plugins[InterpolateIndex].replacements[key] = value
+
+    const DefineIndex = config.plugins.findIndex(e => e.constructor.name === 'DefinePlugin')
+    config.plugins[DefineIndex].definitions['process.env'][key] = `"${value}"`
+}
+
 module.exports = {
     webpack: (config) => {
-        config.output = {
-            ...config.output,
-            filename: 'static/js/bundle.js'
+        config.output = { ...config.output, filename: 'static/js/bundle.js' }
+
+        // set child
+        if (process.env.CRA_PROJECT === 'child') {
+            const canisterId = process.env.CRA_MODE === 'production' ? 'REACT_APP_CHILD_CANISTER_ID' : canisterIds.child.local
+            updateEnvVar(config, 'REACT_APP_CHILD_CANISTER_ID', canisterId)
         }
 
-        // clears canister id env var so it's replaced after a new canister is created
-        // NOTE: plugin indexes change between development and production
-        if (process.env.NODE_ENV === 'production' && process.env.DEPLOY_MODE === 'standalone') {
-            // child canister
-            config.plugins[2].replacements.REACT_APP_CHILD_CANISTER_ID = 'REACT_APP_CHILD_CANISTER_ID'
-            config.plugins[4].definitions['process.env'].REACT_APP_CHILD_CANISTER_ID = '"REACT_APP_CHILD_CANISTER_ID"'
-
-            // parent canister
-            const canisterData = fs.readFileSync('.dfx/local/canister_ids.json', 'utf8')
-            const canisterIds = JSON.parse(canisterData)
-            config.plugins[2].replacements.REACT_APP_PARENT_CANISTER_ID = canisterIds.parent.ic
-            config.plugins[4].definitions['process.env'].REACT_APP_PARENT_CANISTER_ID = `"${canisterIds.parent.ic}"`
-
-            // ledger canister
-            const ledgerCanisterId = 'ryjl3-tyaaa-aaaaa-aaaba-cai' // constant for mainnet
-            config.plugins[2].replacements.REACT_APP_LEDGER_CANISTER_ID = ledgerCanisterId
-            config.plugins[4].definitions['process.env'].REACT_APP_LEDGER_CANISTER_ID = `"${ledgerCanisterId}"`
-
-            // cmc canister
-            const cmcCanisterId = 'rkp4c-7iaaa-aaaaa-aaaca-cai' // constant for mainnet
-            config.plugins[2].replacements.REACT_APP_CMC_CANISTER_ID = cmcCanisterId
-            config.plugins[4].definitions['process.env'].REACT_APP_CMC_CANISTER_ID = `"${cmcCanisterId}"`
+        // set parent
+        if (process.env.CRA_PROJECT === 'parent') {
+            updateEnvVar(config, 'REACT_APP_PARENT_CANISTER_ID', canisterIds.parent.local)
         }
+
+        // set ledger and cmc
+        const ledgerCanisterId = process.env.CRA_MODE === 'development' ? canisterIds.ledger?.local : LEDGER_CANISTER_ID
+        const cmcCanisterId = process.env.CRA_MODE === 'development' ? canisterIds.cmc?.local : CMC_CANISTER_ID
+        updateEnvVar(config, 'REACT_APP_LEDGER_CANISTER_ID', ledgerCanisterId)
+        updateEnvVar(config, 'REACT_APP_CMC_CANISTER_ID', cmcCanisterId)
+
         return config
     },
     paths: (paths, _env) => {
-        const craProject = process.env.CRA_PROJECT
-        const folder = craProject === 'child' ? 'src/_child/frontend' : 'src/_parent/frontend'
+        const folder = process.env.CRA_PROJECT === 'child' ? 'src/_child/frontend' : 'src/_parent/frontend'
         paths.dotenv = path.resolve(__dirname, '.env')
         paths.appIndexJs = path.resolve(__dirname, `${folder}/index.js`)
         paths.appSrc = path.resolve(__dirname, `${folder}`)
         paths.appHtml = path.resolve(__dirname, `${folder}/public/index.html`)
         paths.appPublic = path.resolve(__dirname, `${folder}/public`)
-        paths.appBuild = path.resolve(__dirname, `build/${craProject}`)
+        paths.appBuild = path.resolve(__dirname, `build/${process.env.CRA_PROJECT}`)
         return paths
     },
 }
