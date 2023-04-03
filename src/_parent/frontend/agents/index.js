67c4729ca4e0dabbde27795b0a563e704fdbdfdf
Andreas Tzionis <andy0n7@gmail.com>
2023-04-03 11:29:07 +0300

    removed REACT_APP_ICP_ENV

--- src/_parent/frontend/agents/index.js
+++ src/_parent/frontend/agents/index.js
@@ -1,7 +1,7 @@
 import { HttpAgent } from '@dfinity/agent'
+import { isLocalhost } from '../utils'
 
-const isDev = process.env.REACT_APP_ICP_ENV === 'development'
-
+const isDev = isLocalhost(window.location.host)
 const icHost = isDev ? '127.0.0.1:8000' : 'ic0.app'
 export { icHost }
 
@@ -12,7 +12,7 @@ const getAgent = (identity) => {
   const agent = new HttpAgent({ host: icHost, identity })
 
   // Fetch root key for certificate validation during development
-  if (process.env.REACT_APP_ICP_ENV !== 'production') {
+  if (isDev) {
     agent.fetchRootKey().catch((err) => {
       console.warn('Unable to fetch root key. Check to ensure that your local replica is running')
       console.error(err)
