306e8d08633dd88d59ed1ddeacc333b8b3ffd71f
Andreas Tzionis <andy0n7@gmail.com>
2023-04-04 14:41:23 +0300

    worked on parent frontend ui

--- src/_parent/frontend/pages/Home.js
+++ src/_parent/frontend/pages/Home.js
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from 'react'
-import { Box, Button, Link, Text, useToast, Heading, Spinner, Tag } from '@chakra-ui/react'
+import { Box, Button, Link, Text, useToast, Flex, Spinner, Tag, Heading } from '@chakra-ui/react'
 import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer, } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 import { ParentContext } from '../store/parent'
 import { LedgerContext } from '../store/ledger'
@@ -65,38 +66,60 @@ const Home = () => {
 
 	if (!walletConnected) return <Text>Wallet not connected</Text>
 	return (
-		<Box m="20px">
-			<Box mb="40px">
-				<Button mb="8px" isLoading={loading} disabled={!balance && ledgerCanisterId} onClick={() => createChildBatch()}>Create Child</Button>
-			</Box>
-			<Box>
-				<Heading size={'lg'} mb="20px">Communities</Heading>
-				{childPrincipals ? 
-					childPrincipals?.length > 0 ? <TableContainer>
-						<Table variant='simple'>
-							<TableCaption>All canisters corresponding to <b>{userPrincipal.slice(0, 5)}...{userPrincipal.slice(-3)}</b></TableCaption>
-							<Thead>
-								<Tr>
-									<Th>State</Th>
-									<Th>Canister</Th>
-									<Th>Created At</Th>
-								</Tr>
-							</Thead>
-							<Tbody>
-							{childPrincipals?.map((canister, i) => 
-								<Tr key={i}>
-									<Td><Tag colorScheme={getStateColor(canister.state)}>{canister.state}</Tag></Td>
-									<Td><Link href={getPrincipalUrl(canister.id)} isExternal>
-										{getPrincipalUrl(canister.id)} <ExternalLinkIcon mx='2px' />
-									</Link></Td>
-									<Td><Text>{timeSince(canister.timestamp)}</Text></Td>
-								</Tr>)}
-							</Tbody>
-						</Table>
-					</TableContainer> : 
-					<Text>No canisters yet</Text> : 
-				<Spinner/>}
-			</Box>
+		<Box>
+			<Tabs m="0 auto" maxW="1120px" borderWidth="1px" borderRadius="lg" variant="soft-rounded" colorScheme="gray">
+			<TabList p="20px" borderBottomWidth="1px">
+				<Tab>New Community</Tab>
+				<Tab>User Communities</Tab>
+			</TabList>
+			<TabPanels p="20px">
+				<TabPanel>
+					<Flex p="20px">
+						<Box flex={1}>
+							<img src={require('../public/launch.jpg')} alt="get started communities" width={400}/>
+						</Box>
+						<Flex flex={1} flexDir="column" justifyContent="center" alignItems="center">
+							<Heading size="sm" mb="8px">Join & Grow</Heading>
+							<Heading size="lg" mb="16px">Launch a community</Heading>
+							<Text mb="8px" >✓ Running completely on the Internet Computer</Text>
+							<Text mb="8px"> ✓ Supports Ethereum & Solana authentication </Text>
+							<Text mb="24px"> ✓ One click deploy to user wallet</Text>
+							<Button mb="8px" isLoading={loading} disabled={!balance && ledgerCanisterId} onClick={() => createChildBatch()}>Get Started</Button>
+						</Flex>
+					</Flex>
+				</TabPanel>
+				<TabPanel>
+					<Box>
+						{/* <Heading size={'lg'} mb="20px">Communities</Heading> */}
+						{childPrincipals ? 
+							childPrincipals?.length > 0 ? <TableContainer>
+								<Table variant='simple'>
+									<TableCaption>Communities corresponding to <b>{userPrincipal.slice(0, 5)}...{userPrincipal.slice(-3)}</b></TableCaption>
+									<Thead>
+										<Tr>
+											<Th>State</Th>
+											<Th>Canister</Th>
+											<Th>Created At</Th>
+										</Tr>
+									</Thead>
+									<Tbody>
+									{childPrincipals?.map((canister, i) => 
+										<Tr key={i}>
+											<Td><Tag colorScheme={getStateColor(canister.state)}>{canister.state}</Tag></Td>
+											<Td><Link href={getPrincipalUrl(canister.id)} isExternal>
+												{canister.id} <ExternalLinkIcon mx='2px' />
+											</Link></Td>
+											<Td><Text>{timeSince(canister.timestamp)}</Text></Td>
+										</Tr>)}
+									</Tbody>
+								</Table>
+							</TableContainer> : 
+							<Text>No canisters yet</Text> : 
+						<Spinner/>}
+					</Box>
+				</TabPanel>
+			</TabPanels>
+			</Tabs>
 		</Box>
 	)
 }
