555763179f8660d79427dfccbe483b3ef6b92ca9
Andreas Tzionis <andy0n7@gmail.com>
2023-04-07 16:04:29 +0300

    added cli params to upload scripts

--- src/_parent/upload-assets.js
+++ src/_parent/upload-assets.js
@@ -1,4 +1,5 @@
 const fs = require('fs/promises')
+const minimist = require('minimist')
 const { Actor } = require('@dfinity/agent')
 
 const { getCanisters, getAgent } = require('../_meta/shared/utils')
@@ -6,11 +7,16 @@ const { getFiles, uploadFile } = require('../_meta/shared/assets')
 const { getIdentity } = require('../_meta/shared/identity')
 const { assetFactory } = require('../_meta/shared/idl')
 
+const argv = minimist(process.argv.slice(2))
+const host = argv.network ?? 'http://localhost:8000'
+const id = argv.identity ?? 'default'
+
+// node src/_parent/upload-assets.js --network https://ic0.app 
 ; (async () => {
 
 	const canisters = await getCanisters()
-	const identity = await getIdentity('default')
-	const agent = getAgent('http://localhost:8000', identity)
+	const identity = await getIdentity(id)
+	const agent = getAgent(host, identity)
 	const actor = Actor.createActor(assetFactory, { agent, canisterId: canisters.parent.local })
 
 	// upload wasm
