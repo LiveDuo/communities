ccd3b13f6766c66622956d56d2fb27bc2bdf67ad
Andreas Tzionis <andy0n7@gmail.com>
2023-04-14 19:52:50 +0300

    worked on wallet modal

--- src/_child/frontend/components/modals/WalletModal.js
+++ src/_child/frontend/components/modals/WalletModal.js
@@ -1,10 +1,14 @@
 import { useContext } from 'react'
 import { IdentityContext } from '../../store/identity'
 import { ChildContext } from '../../store/child'
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, Flex } from '@chakra-ui/react'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from '@chakra-ui/react'
 import { Button, Box, Text, Link , Icon} from '@chakra-ui/react'
+
 import { ReactComponent as EthereumLogo } from '../../logos/ethereum.svg'
 import { ReactComponent as SolanaLogo } from '../../logos/solana.svg'
+
+import { ReactComponent as MetamaskLogo } from '../../logos/metamask.svg'
+import { ReactComponent as PhantomLogo } from '../../logos/phantom.svg'
 
 const WalletModal = () => {
   const {isModalOpen, onModalClose, selectedNetwork, setSelectedNetwork, login} = useContext(IdentityContext)
@@ -26,25 +30,43 @@ const WalletModal = () => {
 	return (
 		<Modal isOpen={isModalOpen} onClose={onModalClose} isCentered>
 			<ModalOverlay />
-			<ModalContent minW="600px">
-				<ModalHeader>{!selectedNetwork && (window?.ethereum || window?.solana) ? 'Sign in with...' : 'Download Wallet'} </ModalHeader>
+			<ModalContent minW="480px">
+				<ModalHeader>{!selectedNetwork && (window?.ethereum || window?.solana) ? 'Select a network' : 'You\'d need a wallet'} </ModalHeader>
 				<ModalCloseButton />
 				<ModalBody>
           {selectedNetwork ? 
-            <Box>
-              {selectedNetwork === 'evm' && <Text>Download Metamask <Link href="https://metamask.io/download/" isExternal>download</Link></Text>}
-              {selectedNetwork === 'svm' &&  <Text>Download Phantom <Link href="https://phantom.app/download" isExternal>download</Link></Text>}
+            <Box mb="20px">
+              {selectedNetwork === 'evm' && 
+                <Box mb="12px">
+                  <Text mb="12px">Get an Ethereum wallet </Text>
+                  <Link ml="8px" href="https://metamask.io/download/" isExternal style={{textDecoration: 'none'}}><Button leftIcon={<MetamaskLogo width="24px"/>}>Metamask</Button></Link>
+                </Box>}
+              {selectedNetwork === 'svm' &&  <Box>
+                <Text mb="12px">Get a Solana wallet</Text>
+                <Link ml="8px" href="https://phantom.app/download" isExternal style={{textDecoration: 'none'}}><Button leftIcon={<PhantomLogo width="16px"/>}>Phantom</Button></Link>
+              </Box>}
             </Box> : 
             <Box>
               {(window?.ethereum || window?.solana) ?
-                <Flex justifyContent={'center'}>
-                  {window?.ethereum && <Button leftIcon={<Icon as={EthereumLogo}/>} onClick={() => loginAndSet('evm')}>Ethereum</Button>}
-                  {window?.solana && <Button leftIcon={<Icon as={SolanaLogo}/>} onClick={() => loginAndSet('svm')}>Solana</Button>}
-                </Flex> : 
-                <Flex justifyContent={'center'}>
-                  <Text>Download Metamask <Link href="https://metamask.io/download/" isExternal>download</Link></Text>
-                  <Text>Download Phantom <Link href="https://phantom.app/download" isExternal>download</Link></Text>
-                </Flex>}
+              <Box mb="20px">
+                <Text mb="20px">
+                  Sign in with your wallet. Available wallets:
+                </Text>
+                <Box>
+                  {window?.ethereum && <Button ml="8px" leftIcon={<Icon as={EthereumLogo}/>} onClick={() => loginAndSet('evm')}>Ethereum</Button>}
+                  {window?.solana && <Button ml="8px" leftIcon={<Icon as={SolanaLogo}/>} onClick={() => loginAndSet('svm')}>Solana</Button>}
+                </Box>
+              </Box> :
+                <Box mb="20px">
+                  <Box mb="12px">
+                    <Text mb="12px">Get an Ethereum wallet </Text>
+                    <Link ml="8px" href="https://metamask.io/download/" isExternal style={{textDecoration: 'none'}}><Button leftIcon={<MetamaskLogo width="24px"/>}>Metamask</Button></Link>
+                  </Box>
+                  <Box>
+                    <Text mb="12px">Get a Solana wallet</Text>
+                    <Link ml="8px" href="https://phantom.app/download" isExternal style={{textDecoration: 'none'}}><Button leftIcon={<PhantomLogo width="16px"/>}>Phantom</Button></Link>
+                  </Box>
+              </Box>}
             </Box>}
 				</ModalBody>
 			</ModalContent>
