d053b10d8fd706c1815227d920c83dee4af770c7
Andreas Tzionis <andy0n7@gmail.com>
2023-04-25 20:01:50 +0300

    removed latest path

--- build/build-child.sh
+++ build/build-child.sh
@@ -5,7 +5,7 @@ cargo build --target wasm32-unknown-unknown --package  child --release
 ic-cdk-optimizer target/wasm32-unknown-unknown/release/child.wasm -o target/wasm32-unknown-unknown/release/child_opt.wasm
 
 # copy to build folder
-_VERSION="${VERSION:=latest}"
+_VERSION="${VERSION:="0.0.1"}"
 mkdir -p build/child/$_VERSION
 cp target/wasm32-unknown-unknown/release/child_opt.wasm  build/child/$_VERSION/child.wasm
 
