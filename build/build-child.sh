99beb35aff9749e5d872ac5696d3f68d9aa1c4ca
nikolas-con <nikos.a.constantinou@gmail.com>
2023-04-25 17:32:14 +0300

    fixed folders

--- build/build-child.sh
+++ build/build-child.sh
@@ -2,4 +2,4 @@
 # build child wasm
 cargo build --target wasm32-unknown-unknown --package  child --release
 ic-cdk-optimizer target/wasm32-unknown-unknown/release/child.wasm -o target/wasm32-unknown-unknown/release/child_opt.wasm
-cp target/wasm32-unknown-unknown/release/child_opt.wasm  $SAVE_PATH
+cp target/wasm32-unknown-unknown/release/child_opt.wasm  build/child/$VERSION/child.wasm
