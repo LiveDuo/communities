4f4b1724b85144d983a527864780882554cbca48
nikolas-con <nikos.a.constantinou@gmail.com>
2023-04-25 13:17:14 +0300

    fixed build path for Least and versions

--- build/build-child.sh
+++ build/build-child.sh
@@ -2,5 +2,4 @@
 # build child wasm
 cargo build --target wasm32-unknown-unknown --package  child --release
 ic-cdk-optimizer target/wasm32-unknown-unknown/release/child.wasm -o target/wasm32-unknown-unknown/release/child_opt.wasm
-# cp target/wasm32-unknown-unknown/release/child_opt.wasm build/canister/child.wasm
-cp target/wasm32-unknown-unknown/release/child_opt.wasm build/canister/upgrade/0.0.1/child.wasm
+cp target/wasm32-unknown-unknown/release/child_opt.wasm  $SAVE_PATH
