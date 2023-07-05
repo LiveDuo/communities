bb9941e180d6ea13e725eea965eceff77201f616
Andreas Tzionis <andy0n7@gmail.com>
2023-07-05 16:56:47 +0300

    cleanup how it works

--- src/_parent/frontend/components/landing/HowItWork/index.js
+++ src/_parent/frontend/components/landing/HowItWork/index.js
@@ -3,12 +3,12 @@ import { Container, SimpleGrid, Image, Flex, Heading, Text, Stack } from '@chakr
 const HowItWork = () => {
   return (
     <Container maxW={'5xl'} py={12}>
-      <SimpleGrid mb="20px" mt="20px" columns={{ base: 1, md: 2 }} spacing={10}>
+      <SimpleGrid mb="200px" mt="20px" columns={{ base: 1, md: 2 }} spacing={10}>
         <Stack spacing={4}>
-          <Text textTransform={'uppercase'} color={'blue.400'} fontWeight={600} fontSize={'sm'} bg="blue.50" p={2} alignSelf={'flex-start'} rounded={'md'}>
-            Our Story
+          <Text textTransform={'uppercase'} color={'green.400'} fontWeight={600} fontSize={'sm'} bg="green.50" p={2} alignSelf={'flex-start'} rounded={'md'}>
+            Ownership
           </Text>
-          <Heading>A digital Product design agency</Heading>
+          <Heading>Create a new community</Heading>
           <Text color={'gray.500'} fontSize={'lg'}>Lorem ipsum dolor sit amet consetetur sadi pscing elitr sed diam nonumy eirmod tempor invidunt ut labore</Text>
         </Stack>
         <Flex>
@@ -20,14 +20,7 @@ const HowItWork = () => {
           />
         </Flex>
       </SimpleGrid>
-      <SimpleGrid mb="20px" mt="20px" columns={{ base: 1, md: 2 }} spacing={10}>
-        <Stack spacing={4}>
-          <Text textTransform={'uppercase'} color={'blue.400'} fontWeight={600} fontSize={'sm'} bg="blue.50" p={2} alignSelf={'flex-start'} rounded={'md'}>
-            Our Story
-          </Text>
-          <Heading>A digital Product design agency</Heading>
-          <Text color={'gray.500'} fontSize={'lg'}>Lorem ipsum dolor sit amet consetetur sadi pscing elitr sed diam nonumy eirmod tempor invidunt ut labore</Text>
-        </Stack>
+      <SimpleGrid mb="200px" mt="20px" columns={{ base: 1, md: 2 }} spacing={10}>
         <Flex>
           <Image
             rounded={'md'}
@@ -36,13 +29,20 @@ const HowItWork = () => {
             objectFit={'cover'}
           />
         </Flex>
-      </SimpleGrid>
-      <SimpleGrid mb="20px" mt="20px" columns={{ base: 1, md: 2 }} spacing={10}>
         <Stack spacing={4}>
-          <Text textTransform={'uppercase'} color={'blue.400'} fontWeight={600} fontSize={'sm'} bg="blue.50" p={2} alignSelf={'flex-start'} rounded={'md'}>
-            Our Story
+          <Text textTransform={'uppercase'} color={'green.400'} fontWeight={600} fontSize={'sm'} bg="green.50" p={2} alignSelf={'flex-start'} rounded={'md'}>
+            Maintenance
           </Text>
-          <Heading>A digital Product design agency</Heading>
+          <Heading>No servers involved</Heading>
+          <Text color={'gray.500'} fontSize={'lg'}>Lorem ipsum dolor sit amet consetetur sadi pscing elitr sed diam nonumy eirmod tempor invidunt ut labore</Text>
+        </Stack>
+      </SimpleGrid>
+      <SimpleGrid mb="200px" mt="20px" columns={{ base: 1, md: 2 }} spacing={10}>
+        <Stack spacing={4}>
+          <Text textTransform={'uppercase'} color={'green.400'} fontWeight={600} fontSize={'sm'} bg="green.50" p={2} alignSelf={'flex-start'} rounded={'md'}>
+            Management
+          </Text>
+          <Heading>Tools to nurture and grow</Heading>
           <Text color={'gray.500'} fontSize={'lg'}>Lorem ipsum dolor sit amet consetetur sadi pscing elitr sed diam nonumy eirmod tempor invidunt ut labore</Text>
         </Stack>
         <Flex>
