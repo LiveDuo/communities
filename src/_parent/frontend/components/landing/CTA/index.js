ba563cd6e359dbc8cea55d41957db7e300d5848e
nikolas-con <nikos.a.constantinou@gmail.com>
2023-07-05 18:23:18 +0300

    fixed bg

--- src/_parent/frontend/components/landing/CTA/index.js
+++ src/_parent/frontend/components/landing/CTA/index.js
@@ -1,4 +1,4 @@
-import { Box, Stack, Heading, Text, Container, SimpleGrid } from '@chakra-ui/react'
+import { Box, Stack, Heading, Text, Container, SimpleGrid, useBreakpointValue, Icon } from '@chakra-ui/react'
 
 const CTA = () => {
   return (
@@ -49,8 +49,35 @@ const CTA = () => {
           </Stack>
         </Stack>
       </Container>
+      <Blur
+        position={'absolute'}
+        top={-10}
+        left={-10}
+        style={{ filter: 'blur(70px)' }}
+      />
     </Box>
   );
 }
 
 export default CTA
+
+const Blur = (props) => {
+  return (
+    <Icon
+      width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
+      zIndex={-1}
+      height="560px"
+      viewBox="0 0 528 560"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <circle cx="71" cy="61" r="111" fill="#F56565" />
+      <circle cx="244" cy="106" r="139" fill="#ED64A6" />
+      <circle cy="291" r="139" fill="#ED64A6" />
+      <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
+      <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
+      <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
+      <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
+    </Icon>
+  )
+}
