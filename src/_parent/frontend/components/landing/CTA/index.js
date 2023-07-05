51937e0ffd91f51968b36788fee47b177f2dbeee
Andreas Tzionis <andy0n7@gmail.com>
2023-07-05 18:49:22 +0300

    worked on cta

--- src/_parent/frontend/components/landing/CTA/index.js
+++ src/_parent/frontend/components/landing/CTA/index.js
@@ -1,60 +1,44 @@
-import { Box, Stack, Heading, Text, Container, SimpleGrid, useBreakpointValue, Icon } from '@chakra-ui/react'
+import { Box, Stack, Heading, Text, Container, SimpleGrid, useBreakpointValue, Icon, Button } from '@chakra-ui/react'
 
 const CTA = () => {
   return (
     <Box position={'relative'}>
-      <Container
-        as={SimpleGrid}
-        maxW={'7xl'}
-        columns={{ base: 1, md: 2 }}
-        spacing={{ base: 10, lg: 32 }}
-        py={{ base: 10, sm: 20, lg: 32 }}>
+      <Container as={SimpleGrid} maxW={'7xl'} columns={{ base: 1, md: 2 }} spacing={{ base: 10, lg: 32 }} py={{ base: 10, sm: 20, lg: 32 }}>
         <Stack spacing={{ base: 10, md: 20 }}>
-          <Heading
-            lineHeight={1.1}
-            fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}>
-            Senior web designers{' '}
-            <Text
-              as={'span'}
-              bgGradient={'linear(to-r, red.400,pink.400)'}
-              bgClip="text">
-              &
-            </Text>{' '}
-            Full-Stack Developers
+          <Heading lineHeight={1.1} fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}>
+            <Box mt="60px" mb="30px">
+              Digital <Text as={'span'} color="white" bgColor="green.300">&nbsp;spaces&nbsp;</Text>
+            </Box>
+            <Box>
+              <Text as={'span'} color="green.300" bgColor="white">&nbsp;Owned&nbsp;</Text> by creators
+            </Box>
           </Heading>
         </Stack>
-        <Stack
-          bg={'gray.50'}
-          rounded={'xl'}
-          p={{ base: 4, sm: 6, md: 8 }}
-          spacing={{ base: 8 }}
-          maxW={{ lg: 'lg' }}>
+        <Stack bg={'gray.50'} rounded={'xl'} p={{ base: 4, sm: 6, md: 8 }} spacing={{ base: 8 }} maxW={{ lg: 'lg' }}>
           <Stack spacing={4}>
-            <Heading
-              color={'gray.800'}
-              lineHeight={1.1}
-              fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
-              Join our team
-              <Text
-                as={'span'}
-                bgGradient="linear(to-r, red.400,pink.400)"
-                bgClip="text">
-                !
-              </Text>
+            <Heading color={'gray.800'} lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
+              Launch a community
             </Heading>
             <Text color={'gray.500'} fontSize={{ base: 'sm', sm: 'md' }}>
-              We’re looking for amazing engineers just like you! Become a part
-              of our rockstar engineering team and skyrocket your career!
+              <br/>
+              ✓ Running completely on the Internet Computer
+              <br/>
+              <br/>
+              ✓ Supports Ethereum & Solana logins
+              <br/>
+              <br/>
+              ✓ One click deploy to user wallet
+              <br/>
+              <br/>
             </Text>
+
+            <Button colorScheme={'green'} bg={'green.400'} rounded={'full'} px={6} _hover={{ bg: 'green.500' }}>
+              Get Started
+            </Button>
           </Stack>
         </Stack>
       </Container>
-      <Blur
-        position={'absolute'}
-        top={-10}
-        left={-10}
-        style={{ filter: 'blur(70px)' }}
-      />
+      <Blur position={'absolute'} top={-10} left={-10} style={{ filter: 'blur(70px)' }}/>
     </Box>
   );
 }
