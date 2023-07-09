eb9f244bed37557a14571ff9fb4be3322e4b8d79
Andreas Tzionis <andy0n7@gmail.com>
2023-07-10 00:03:40 +0300

    minor fix

--- src/_parent/frontend/components/landing/Navbar/DesktopNav.js
+++ src/_parent/frontend/components/landing/Navbar/DesktopNav.js
@@ -4,14 +4,14 @@ import { ChevronRightIcon } from '@chakra-ui/icons';
 
 const DesktopSubNav = ({ label, href, subLabel }) => {
   return (
-    <Link href={href} role={'group'} display={'block'} p={2} rounded={'md'} _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+    <Link href={href} role={'group'} display={'block'} p={2} rounded={'md'} _hover={{ bg: useColorModeValue('green.50', 'green.900') }}>
       <Stack direction={'row'} align={'center'}>
         <Box>
-          <Text transition={'all .3s ease'} _groupHover={{ color: 'pink.400' }} fontWeight={500}>{label}</Text>
+          <Text transition={'all .3s ease'} _groupHover={{ color: 'green.400' }} fontWeight={500}>{label}</Text>
           <Text fontSize={'sm'}>{subLabel}</Text>
         </Box>
         <Flex transition={'all .3s ease'} transform={'translateX(-10px)'} opacity={0} _groupHover={{ opacity: '100%', transform: 'translateX(0)' }} justify={'flex-end'} align={'center'} flex={1}>
-          <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+          <Icon color={'green.400'} w={5} h={5} as={ChevronRightIcon} />
         </Flex>
       </Stack>
     </Link>
