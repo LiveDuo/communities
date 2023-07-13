e0ccec02b3c141939b89e13567c43beb734bc196
Andreas Tzionis <andy0n7@gmail.com>
2023-07-13 20:17:43 +0300

    fixed feature icons

--- src/_parent/frontend/components/landing/Features/index.js
+++ src/_parent/frontend/components/landing/Features/index.js
@@ -1,14 +1,11 @@
-import { Box, Container, Flex, Heading, Icon, Stack, Text, useColorModeValue } from '@chakra-ui/react';
-import { FcAbout, FcAssistant, FcCollaboration, FcDonate, FcManager, } from 'react-icons/fc';
+import { Box, Container, Flex, Heading, Stack, Text, Image } from '@chakra-ui/react';
 
-const Card = ({ heading, description, icon }) => {
+const Card = ({ heading, description, image }) => {
   return (
     <Box maxW={{ base: 'full', md: '275px' }} w={'full'} borderWidth="1px" borderRadius="lg" overflow="hidden" p={5} textAlign="center">
       <Stack align={'center'} spacing={2}>
-        <Flex w={16} h={16} mb="12px" align={'center'} justify={'center'} color={'white'} rounded={'full'} bg={useColorModeValue('gray.100', 'gray.700')}>
-          {icon}
-        </Flex>
-        <Box mt={2}>
+          {image}
+        <Box mt={6}>
           <Heading size="md">{heading}</Heading>
           <Text mt={1} fontSize={'sm'}>{description}</Text>
         </Box>
@@ -34,27 +31,27 @@ const Features = () =>  {
         <Flex flexWrap="wrap" gridGap={6} justify="center">
           <Card
             heading={'Wallet Login'}
-            icon={<Icon as={FcAssistant} w={10} h={10} />}
+            image={<Image src={require('../../../public/backpack-icon.png')} alt={'feature icon'} width="160px"/>}
             description={'Login with Metamask, Phantom or Plug wallet.'}
           />
           <Card
             heading={'Free for users'}
-            icon={<Icon as={FcCollaboration} w={10} h={10} />}
+            image={<Image src={require('../../../public/bag-icon.png')} alt={'feature icon'} width="160px"/>}
             description={'Users don\'t have to pay transaction fees to interact.'}
           />
           <Card
             heading={'Owned by creators'}
-            icon={<Icon as={FcDonate} w={10} h={10} />}
+            image={<Image src={require('../../../public/ball-icon.png')} alt={'feature icon'} width="160px"/>}
             description={'Creators have 100% ownership of the communities they create.'}
           />
           <Card
             heading={'Runs on blockchain'}
-            icon={<Icon as={FcManager} w={10} h={10} />}
+            image={<Image src={require('../../../public/plants-icon.png')} alt={'feature icon'} width="160px"/>}
             description={'All actions are carried out by 13 nodes.'}
           />
           <Card
             heading={'Easy to deploy'}
-            icon={<Icon as={FcAbout} w={10} h={10} />}
+            image={<Image src={require('../../../public/coffee-icon.png')} alt={'feature icon'} width="160px"/>}
             description={'You can create new communities with only one click.'}
           />
         </Flex>
