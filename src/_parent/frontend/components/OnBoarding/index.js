2e5a54788e992eee3d3e0ce2b9c2dab09cdb7188
Andreas Tzionis <andy0n7@gmail.com>
2023-07-13 17:09:41 +0300

    various fixes

--- src/_parent/frontend/components/OnBoarding/index.js
+++ src/_parent/frontend/components/OnBoarding/index.js
@@ -11,12 +11,10 @@ import { isLocal } from '../../agents'
 const chromeStoreUrl = 'https://chrome.google.com/webstore/detail/plug/cfbfdhimifdmdehjmkdobpcjfefblkjm'
 
 // https://www.blobmaker.app/
-const BlobBackground1 = ({fill}) => (
-  <Box position="absolute" width="100%" height="100%">
-    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-      <path fill={fill} d="M26.2,-28.4C30.6,-21.8,28.5,-10.9,28.2,-0.3C27.9,10.3,29.3,20.6,24.9,30.7C20.6,40.8,10.3,50.8,-0.5,51.3C-11.3,51.8,-22.7,42.9,-37.3,32.8C-51.9,22.7,-69.8,11.3,-70.2,-0.4C-70.6,-12.1,-53.4,-24.2,-38.8,-30.8C-24.2,-37.4,-12.1,-38.5,-0.6,-37.9C10.9,-37.3,21.8,-35,26.2,-28.4Z" transform="translate(120 70), scale(1.5)" />
-    </svg>
-  </Box>
+const BlobBackground = ({fill}) => (
+  <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+    <path fill={fill} d="M26.2,-28.4C30.6,-21.8,28.5,-10.9,28.2,-0.3C27.9,10.3,29.3,20.6,24.9,30.7C20.6,40.8,10.3,50.8,-0.5,51.3C-11.3,51.8,-22.7,42.9,-37.3,32.8C-51.9,22.7,-69.8,11.3,-70.2,-0.4C-70.6,-12.1,-53.4,-24.2,-38.8,-30.8C-24.2,-37.4,-12.1,-38.5,-0.6,-37.9C10.9,-37.3,21.8,-35,26.2,-28.4Z" transform="translate(120 70), scale(1.5)" />
+  </svg>
 )
 
 const OnBoarding = ({ createChildBatch }) => {
@@ -46,7 +44,9 @@ const OnBoarding = ({ createChildBatch }) => {
         <Flex position="relative">
           <Box flex={1} position="relative">
             <Box position="relative">
-              <BlobBackground1 fill={"#F2F4F8"}/>
+              <Box position="absolute" width="100%" height="100%">
+                <BlobBackground fill={"#F2F4F8"}/>
+              </Box>
             </Box>
             <Flex position="relative" justifyContent="center">
               <img src={require('../../public/download-wallet.png')} alt="get started communities" width={'50%'}/>
@@ -65,18 +65,20 @@ const OnBoarding = ({ createChildBatch }) => {
         <Flex>
           <Box flex={1} position="relative">
             <Box position="relative">
-              <BlobBackground1 fill={"#F2F4F8"}/>
+              <Box position="absolute" width="100%" height="100%">
+                <BlobBackground fill={"#F2F4F8"}/>
+              </Box>
             </Box>
             <Flex position="relative" justifyContent="center" mt="20px">
               <img src={require('../../public/connect-wallet.png')} alt="get started communities" width={'50%'}/>
             </Flex>
           </Box>
           <Flex flex={1} flexDir="column" justifyContent="center" alignItems="center" mb="12px">
-            <Heading size="lg" mb="16px">Connect your wallet</Heading>
-            <Text mb="8px" >✓ Running completely on the Internet Computer</Text>
-            <Text mb="8px"> ✓ Supports Ethereum & Solana authentication </Text>
-            <Text mb="24px"> ✓ One click deploy to user wallet</Text>
-            <Button mb="8px" isLoading={loading} onClick={connect}>Connect wallet</Button>
+            <Heading size="lg" mb="32px">Connect your wallet</Heading>
+            <Text mb="16px" >Your community is about to be deployed</Text>
+            <Text mb="32px"> Ownership will be transferred to your wallet</Text>
+            <Button mb="24px" isLoading={loading} onClick={connect}>Connect wallet</Button>
+            <Text fontSize="sm" color='gray'> <b>Note:</b> You can change the owner wallet later</Text>
           </Flex>
         </Flex>
       )}
