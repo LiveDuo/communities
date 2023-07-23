c96de5e1d0485713133ab9c6a794180f2c2e7851
Andreas Tzionis <andy0n7@gmail.com>
2023-07-23 21:50:11 +0300

    worked on identity store

--- src/_parent/frontend/store/identity.js
+++ src/_parent/frontend/store/identity.js
@@ -9,50 +9,50 @@ import { isLocal } from '../utils/url'
 
 const IdentityContext = createContext()
 
+const wallet_name = 'plug' // or 'plug'
+const wallet_object = window.ic?.[wallet_name]
+
 const IdentityProvider = ({ children }) => {
 
 	const [walletConnected, setWalletConnected] = useState(false)
 	const [walletDetected, setWalletDetected] = useState(false)
 	const [userPrincipal, setUserPrincipal] = useState('')
-	const [host, setHost] = useState('')
 	const toast = useToast()
 	const walletDisclosure = useDisclosure()
 
 	const loadWallet = useCallback(async () => {
 		
 		// check connected
-		const connected = await window.ic.plug.isConnected()
+		const connected = await wallet_object.isConnected()
 		if (!connected) return
 
 		// load params
-		const principal = await window.ic?.plug.getPrincipal()
+		const principal = await wallet_object.getPrincipal()
 		setUserPrincipal(principal.toString())
-		setHost(window.ic?.plug.sessionManager.host)
 		setWalletConnected(true)
 	
 	}, [])
 
 	useEffect(() => {
-		setWalletDetected(!!window?.ic?.plug)
+		setWalletDetected(!!wallet_object)
 	}, [])
 	
 	const createActor = (...params) => {
-		return window.ic?.plug.createActor(...params)
+		return wallet_object.createActor(...params)
 	}
 
 	const batchTransactions = (...params) => {
-		return window.ic.plug.batchTransactions(...params)
+		return wallet_object.batchTransactions(...params)
 	}
 
 	const connect = async () => {
 		const host = isLocal === 'localhost' ? 'http://127.0.0.1:8000/' : 'https://mainnet.dfinity.network'
 		const whitelist = ledgerCanisterId ? [ledgerCanisterId, parentCanisterId] : [parentCanisterId]
 		try {
-			const hasAllowed = await window.ic?.plug?.requestConnect({ host, whitelist })
-			const principal = await window.ic?.plug.getPrincipal()
+			const hasAllowed = await wallet_object?.requestConnect({ host, whitelist })
+			const principal = await wallet_object?.getPrincipal()
 			setUserPrincipal(principal.toString())
 			setWalletConnected(!!hasAllowed)
-			setHost(window.ic?.plug.sessionManager.host)
 			toast({ description: 'Connected' })
 		} catch (error) {
 			console.log(error.message)
@@ -66,11 +66,10 @@ const IdentityProvider = ({ children }) => {
 
 	const disconnect = async () => {
 		// not resolving
-		// await window.ic.plug.disconnect()
+		// await wallet_object.disconnect()
 
 		setUserPrincipal('')
 		setWalletConnected(false)
-		setHost('')
 
 		toast({ description: 'Disconnected' })
 	}
@@ -81,7 +80,7 @@ const IdentityProvider = ({ children }) => {
 		}
 	},[loadWallet, walletDetected])
 
-	const value = { walletDisclosure, createActor, userPrincipal, host, connect, disconnect, loadWallet, walletConnected, walletDetected, batchTransactions }
+	const value = { walletDisclosure, createActor, userPrincipal, connect, disconnect, loadWallet, walletConnected, walletDetected, batchTransactions }
 
 	return (
 		<IdentityContext.Provider value={value}>
