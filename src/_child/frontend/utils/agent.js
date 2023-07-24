f640d33a4b0e3e2b2846c32f93c56fbe403f930c
nikolas-con <nikos.a.constantinou@gmail.com>
2023-07-24 16:38:06 +0300

    splitted agent index file

---
+++ src/_child/frontend/utils/agent.js
@@ -0,0 +1,19 @@
+import { HttpAgent } from '@dfinity/agent'
+import { isLocal } from './url'
+
+const icHost = isLocal ? '127.0.0.1:8000' : 'ic0.app'
+export { icHost }
+
+const getAgent = (identity) => {
+  const agent = new HttpAgent({ host: icHost, identity })
+
+  // Fetch root key for certificate validation during development
+  if (isLocal) {
+    agent.fetchRootKey().catch((err) => {
+      console.warn('Unable to fetch root key. Check to ensure that your local replica is running')
+      console.error(err)
+    })
+  }
+  return agent
+}
+export { getAgent }
