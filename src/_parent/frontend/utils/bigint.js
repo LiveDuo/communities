e3f6e11fa19cc5aa432b9c40e2a8c7b35b2b424a
Andreas Tzionis <andy0n7@gmail.com>
2023-09-11 13:43:32 +0300

    minor fix

--- src/_parent/frontend/utils/bigint.js
+++ src/_parent/frontend/utils/bigint.js
@@ -1,5 +1,7 @@
-const BIG_PRECISION = 3 // to multiply a bigint with floats
 /* global BigInt */
+
+const BIG_PRECISION = 3 // to multiply a bigint with floats
+
 const getBigIntAllowance = (number, zeros, allowance) => {
 	const billion_cycles_with_allowance = BigInt(10 ** zeros) * BigInt(Math.floor((1 + allowance) * (10 ** BIG_PRECISION))) / BigInt(10 ** BIG_PRECISION)
 	return BigInt(number) * billion_cycles_with_allowance
