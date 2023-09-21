b17a38ceab8d4c1b7776f233f8a31cb4c7af3425
nikolas-con <nikos.a.constantinou@gmail.com>
2023-09-21 11:35:25 +0300

    update management script

--- .notes/wallet/management.js
+++ .notes/wallet/management.js
@@ -7,19 +7,18 @@ const { getIdentity } = require('../../src/_meta/shared/identity')
 const { managementFactory } = require('../../src/_meta/shared/idl')
 
 const host = 'https://ic0.app'
+const MANAGEMENT_CANISTER_ID = Principal.fromText('aaaaa-aa');
 
+// https://forum.dfinity.org/t/how-to-make-ic-management-api-canister-status-call-with-dfinity-agent/7139/2
 // node .notes/wallet/management.js
-; (async () => {
+;(async () => {
 
 	const identity = await getIdentity('default')
 	const agent = getAgent(host, identity)
-	const actor = Actor.createActor(managementFactory, { agent, canisterId: 'aaaaa-aa' })
-
 	const canisterId = Principal.fromText('y3ouo-yaaaa-aaaap-qbafa-cai')
-	const res = await actor.canister_info({canister_id: canisterId, num_requested_changes: [] })
-	console.log(res)
+
+	const actor = Actor.createActor(managementFactory, { agent, canisterId: MANAGEMENT_CANISTER_ID, effectiveCanisterId: canisterId })
 	
 	const res2 = await actor.canister_status({canister_id: canisterId })
 	console.log(res2)
-
 })()
