26f75aab31d5e7fc2af2bd6cc02b9666e9ffbb01
Andreas Tzionis <andy0n7@gmail.com>
2024-04-05 22:47:11 +0300

    worked on profile page

--- src/_child/frontend/pages/Profile.js
+++ src/_child/frontend/pages/Profile.js
@@ -1,9 +1,9 @@
 import { useEffect, useContext, useState, useCallback } from 'react'
 import { useParams } from 'react-router-dom'
-import { Box, Text, Badge } from '@chakra-ui/react'
+import { Box, Text, Badge, Link } from '@chakra-ui/react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
-import { capitalizeFirstLetter, getSeedFromAccount } from '../utils/address'
+import { capitalizeFirstLetter, getSeedFromAccount, getExplorerUrl, getAuthentication } from '../utils/address'
 import EntriesContainer from '../components/containers/entries'
 import { IdentityContext } from '../store/identity'
 import { ChildContext } from '../store/child'
@@ -51,8 +51,23 @@ const Profile = () => {
         <Box mb="20px">
           <Jazzicon diameter={60} seed={account ? getSeedFromAccount(account) : ''} />
         </Box>
-        <Text mb="20px">{address}</Text>
-        <Badge fontSize={'md'}>{type?.toUpperCase()}</Badge>
+        <Box mb="20px">
+          <Box mr="8px" display="inline">
+            <Badge fontSize={'md'}>{type?.toUpperCase()}</Badge>
+          </Box>
+          {type && address && <Link href={getExplorerUrl(getAuthentication(address, capitalizeFirstLetter(type)))} isExternal>
+            <Text display="inline" mb="20px">{address}</Text>
+          </Link>}
+        </Box>
+        <Box mb="20px">
+          <Box display="inline">
+            <Badge mr="8px" fontSize={'xs'}>Last login: 2/2/22</Badge>
+            <Badge mr="8px" fontSize={'xs'}>Join date: 3/3/23</Badge>
+            <Badge mr="8px" fontSize={'xs'}>Total posts: 13</Badge>
+            <Badge mr="8px" fontSize={'xs'}>Total replies: 4</Badge>
+            <Badge fontSize={'xs'}>Total likes: 23</Badge>
+          </Box>
+        </Box>
       </Box>
 
       <Tabs>
